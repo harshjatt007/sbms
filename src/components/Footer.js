3212214163bf7c656e092fdcdcfa39919a73c0f4
Abhishek Choudhary <abhishekchoudhary236@gmail.com>
2024-08-05 18:03:58 +0530

    content added more

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,7 +44,7 @@ export default function Footer() {
           rel="noreferrer"
           className="underline"
         >
-          Thomas Sankara
+          Abhhishek Choudhary
         </a>
       </p>
     </>
